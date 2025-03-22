f9ccb9f1182f617cc59cb6c3f6eb482bdb6f9284
devgev <leifminfagel@gmail.com>
2025-03-22 11:17:45 +0100

    tests: fix incorrect url test

--- tests/url/url.cpp
+++ tests/url/url.cpp
@@ -5,12 +5,12 @@
 
 #define TEST_NAME "uri/url.cpp"
 
-#define TEST_URL_STRING "https://subdomain.domain.com/a/a/d/f/abc%4112=1231%0501-.3=1" 
+#define TEST_URL_STRING "https://subdomain.domain.com/a/a/d/f/abc?4112=1231%0501-.3=1"
 #define EXPECTED_PROTOCOL torr::url::protocol_type::https
 #define EXPECTED_HOST "subdomain.domain.com" 
 #define EXPECTED_PORT 443
-#define EXPECTED_PATH "/a/a/d/f/"
-#define EXPECTED_QUERY "abc%4112=1231%0501-.3=1"
+#define EXPECTED_PATH "/a/a/d/f/abc"
+#define EXPECTED_QUERY "?4112=1231%0501-.3=1"
 
 #define TEST_URL_STRING2 "https://a.a.com:11443501"
 #define EXPECTED_PORT2 11443501
@@ -48,7 +48,7 @@ int main()
     );
 
     assert(
-        url.from_string(std::string(TEST_URL_STRING2)).value().port() == EXPECTED_PORT2 &&
+        url.from_string(std::string(TEST_URL_STRING2)).value()->port() == EXPECTED_PORT2 &&
         "failed due to url.port()"
     );
 
